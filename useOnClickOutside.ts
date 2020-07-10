6db29d62bded8ead7d3f6e84be1d560b3d962611
janKozie1 <jankkoziel@gmail.com>
2020-07-10 23:36:16 +0200

    new hooks

---
+++ useOnClickOutside.ts
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const useOnClickOutside = <T extends HTMLElement>(
+    ref: React.RefObject<T>,
+    callback: (e: React.MouseEvent<T, 'click'>) => void
+) => {
+    const savedCallback = React.useRef(callback)
+
+    React.useEffect(() => {
+        savedCallback.current = callback
+    }, [callback])
+
+    React.useEffect(() => {
+        const { current: nodeToAttachTo } = ref
+
+        if (!nodeToAttachTo) {
+            return
+        }
+
+        const call = (e: React.MouseEvent<T, 'click'>) => {
+            savedCallback.current(e)
+        }
+
+        const handler = (e: React.MouseEvent<T, 'click'>) => {
+            const { target } = e
+            console.log(target)
+            if (
+                !(target instanceof window.Element) ||
+                nodeToAttachTo.contains(target)
+            ) {
+                return
+            }
+
+            call(e)
+        }
+        console.log('owo')
+        document.addEventListener(
+            'click',
+            (handler as unknown) as EventListener
+        )
+        return () =>
+            document.removeEventListener(
+                'click',
+                (handler as unknown) as EventListener
+            )
+    }, [ref])
+}
+
+export default useOnClickOutside
